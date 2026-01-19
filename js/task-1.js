61e650e47a1fa14ae307489d973a98b098d5b371
alinarozental <alina.rosenthal33@icloud.com>
2026-01-19 09:48:51 +0100

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,10 @@
 const totalCategoriesList = document.querySelectorAll('#categories .item');
 console.log('Загальна кількість категорій:', totalCategoriesList.length);
+
+totalCategoriesList.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const count = item.querySelectorAll('li').length;
+
+  console.log(title);
+  console.log(count);
+});
