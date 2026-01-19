09b58e65f831be8c7f3b70ce3d86db2ea3c92bbe
alinarozental <alina.rosenthal33@icloud.com>
2026-01-19 19:07:57 +0100

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventName('input', () => {
+  const trimValue = nameInput.value.trim();
+
+  if (trimValue === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = trimValue;
+  }
+});
