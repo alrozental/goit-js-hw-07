7794f3aa46d3acd3e40a8bd350b6217024693511
alinarozental <alina.rosenthal33@icloud.com>
2026-01-19 19:56:35 +0100

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.body;
+const colorSpan = document.querySelector('.color');
+
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+colorSpan.textContent = randomColor;
+console.log(randomColor);
+});
